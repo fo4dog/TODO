06430d1cc0c40df3a627e1d47f7f496e7dc1a127
fo4dog <7249901@gmail.com>
2023-03-06 16:49:16 +0300

    HW_5 ready

--- TODO/users/views.py
+++ TODO/users/views.py
@@ -1,11 +1,13 @@
-from rest_framework.renderers import JSONRenderer
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
-
-
-from .models import User
 from .serializers import UserModelSerializer
+from .models import User
 
 
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
     serializer_class = UserModelSerializer
+    queryset = User.objects.all()
